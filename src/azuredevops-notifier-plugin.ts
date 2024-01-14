09c5130d899ecc68fd7133cd7ac5059ca9f5c54a
harunari <ryo13579978@gmail.com>
2024-01-14 23:38:19 +0900

    :fire: Remove commit validations

--- src/azuredevops-notifier-plugin.ts
+++ src/azuredevops-notifier-plugin.ts
@@ -1,8 +1,5 @@
 import fetch from 'node-fetch';
-import path from 'node:path'; 
-import { HEAD, Repository } from 'tiny-commit-walker';
 import type { NotifierPlugin, NotifyParams, PluginCreateOptions, PluginLogger } from 'reg-suit-interface';
-import { fsUtil } from 'reg-suit-util';
 import { createCommentBody } from './create-comment';
 
 export interface AzureDevopsPluginOption {
@@ -13,30 +10,15 @@ export interface AzureDevopsPluginOption {
 export class AzureDevopsNotifierPlugin implements NotifierPlugin<AzureDevopsPluginOption> {
   private logger!: PluginLogger;
   private isNoEmit!: boolean;
-  private repo!: Repository;
   private options!: AzureDevopsPluginOption;
 
   init(config: PluginCreateOptions<AzureDevopsPluginOption>): void {
     this.logger = config.logger;
     this.isNoEmit = config.noEmit;
-    this.repo = new Repository(path.join(fsUtil.prjRootDir('.git'), '.git'));
     this.options = config.options;
   }
 
   async notify(params: NotifyParams): Promise<void> {
-    const head = this.repo.readHeadSync();
-    const commitHash = this.getCommitHash(head);
-    if (!commitHash) {
-      this.logger?.error('Can\'t detect HEAD branch or commit.');
-      return Promise.reject();
-    }
-
-    if (head.type !== 'branch') {
-      this.logger.warn('HEAD is not attached into any branches.');
-      return Promise.reject();
-    }
-
-    this.logger.info(`Comment to PR associated with ${this.logger.colors.green(head?.branch?.name ?? '')} .`);
     const { failedItems, newItems, deletedItems, passedItems } = params.comparisonResult;
     const comment = createCommentBody({
       reportUrl: params.reportUrl,
@@ -90,14 +72,6 @@ export class AzureDevopsNotifierPlugin implements NotifierPlugin<AzureDevopsPlug
     }
     finally {
       spinner.stop();
-    }
-  }
-
-  private getCommitHash(head: HEAD): string | undefined {
-    if (head.type === 'branch') {
-      return head.branch?.commit.hash;
-    } else if (head.type === 'commit') {
-      return head.commit?.hash;
     }
   }
 }
